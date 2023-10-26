cc24335f5ec99176cf78fa3dececc1a05cd39aab
Kevin Neaton <kevin@neaton.xyz>
2023-10-26 12:22:37 -0400

    feat: read enabled oauth providers from env

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -10,6 +10,7 @@ import { Label } from "@/components/ui/label"
 import { signInWithOAuth, signInWithOtp } from "@/app/auth/actions"
 import { useFormState, useFormStatus } from "@/lib/react-dom-shim"
 import Link from "next/link"
+import { z } from "zod"
 
 
 const initialState = {
@@ -56,26 +57,85 @@ function EmailAuthForm() {
   )
 }
 
+
+ // Includes all providers currently supported by supabase-js
+const SupabaseOAuthProvider = z.enum([
+  'apple',
+  'azure',
+  'bitbucket',
+  'discord',
+  'facebook',
+  'figma',
+  'github',
+  'gitlab',
+  'google',
+  'kakao',
+  'keycloak',
+  'linkedin',
+  // 'linkedin_oidc',
+  'notion',
+  'slack',
+  'spotify',
+  'twitch',
+  'twitter',
+  'workos',
+  'zoom',
+  'fly'
+])
+
+const ProviderArray = z.array(SupabaseOAuthProvider)
+const stringToArrays = z.string().transform((val) => val.split(/[, ]+/).filter(Boolean))
+const enabledOAuthProviders = ProviderArray.parse(
+  stringToArrays.parse(process.env.NEXT_PUBLIC_OAUTH_PROVIDERS)
+)
+
+// TODO add more icons
+const providerIcons = {
+  'github': Icons.gitHub,
+  'google': Icons.google,
+  'apple': Icons.apple,
+  // 'azure': Icons.azure,
+  // 'bitbucket': Icons.bitbucket,
+  // 'discord': Icons.discord,
+  // 'facebook': Icons.facebook,
+  // 'figma': Icons.figma,
+  // 'gitlab': Icons.gitlab,
+  // 'kakao': Icons.kakao,
+  // 'keycloak': Icons.keycloak,
+  // 'linkedin': Icons.linkedin,
+  // 'notion': Icons.notion,
+  // 'slack': Icons.slack,
+  // 'spotify': Icons.spotify,
+  // 'twitch': Icons.twitch,
+  'twitter': Icons.twitter,
+  // 'workos': Icons.workos,
+  // 'zoom': Icons.zoom,
+  // 'fly': Icons.fly
+};
+
+function ProviderIcon({ provider }: { provider: string }) {
+  const Icon = provider in providerIcons ? 
+    providerIcons[provider as keyof typeof providerIcons] :
+    null
+
+  return Icon && <Icon className="mr-2 h-4 w-4" />
+}
+
 function SocialAuthFormFields() {
   const { pending } = useFormStatus()
+
   return (
     <>
-      <Button type="submit" name="provider" value="github" variant="outline" disabled={pending}>
-        {pending ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.gitHub className="mr-2 h-4 w-4" />
-        )}{" "}
-        Github
-      </Button>
-      <Button type="submit" name="provider" value="google" variant="outline" disabled={pending}>
-        {pending ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.google className="mr-2 h-4 w-4" />
-        )}{" "}
-        Google
-      </Button>
+      {enabledOAuthProviders.map((provider) => (
+        <Button key={provider} type="submit" name="provider" value={provider} variant="outline" disabled={pending}>
+          {pending ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <ProviderIcon provider={provider} />
+          )}{" "}
+          {provider.charAt(0).toUpperCase() + provider.slice(1)}
+        </Button>
+      ))}
     </>
   )
 }
@@ -104,19 +164,23 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
       <div className="grid gap-1">
         <EmailAuthForm />
       </div>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <div className="grid gap-1">
-        <SocialAuthForm />
-      </div>
+      {enabledOAuthProviders.length > 0 && (
+        <>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="grid gap-1">
+            <SocialAuthForm />
+          </div>
+        </>
+      )}
       <p className="px-8 text-center text-sm text-muted-foreground">
         By clicking continue, you agree to our{" "}
         <Link
