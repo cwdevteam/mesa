c6a23fa81851667da7da7f539ba8065a57de120a
Kevin Neaton <kevin@neaton.xyz>
2023-10-28 02:11:34 -0400

    feat: add initial mesa branding

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -5,18 +5,6 @@ import UserAuthForm from '@/components/UserAuthForm'
 export default async function Home() {
   return (
     <main className="grid content-start justify-center gap-6 sm:gap-12 md:gap-24 pt-10 pb-4">
-      <div className="container text-center hidden">
-        <section className="grid place-items-center max-w-[800px] gap-2 px-4 py-8 md:pt-16 lg:pt-20">
-          <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
-            The fastest way to start building apps with Supabase and Next.js
-          </h1>
-          <p className="max-w-[600px] pl-1 text-lg text-muted-foreground sm:text-xl">
-            Kickstart your projects with this Next.js starter, featuring{' '}
-            <Link className="underline text-primary" href="https://supabase.com">Supabase Auth</Link> and{' '}
-            <Link className="underline text-primary" href="https://ui.shadcn.com/">shadcn/ui</Link> components.
-          </p>
-        </section>
-      </div>
       <div className="container w-fit">
         <section>
           <UserAuthForm />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,14 @@
 import Link from "next/link";
 
 import { ThemeToggle } from "@/components/ThemeToggle"
-import { Icons } from "@/components/Icons"
+import { Logo } from "@/components/Logo"
 
 export default function Header() {
   return (
     <header className="flex border-b border-foreground/20">
       <div className="flex container mx-auto py-4">
         <Link className="flex items-center gap-2" href="/">
-          <Icons.logo className="h-6 w-6" />
-          <span className="hidden font-bold sm:inline-block">Next.js</span>
-          <span className="hidden font-bold sm:inline-block">+</span>
-          <span className="hidden font-bold sm:inline-block">shadcn/ui</span>
+          <Logo className="h-6 w-auto" />
         </Link>
         <div className="ml-auto">
           <ThemeToggle />
