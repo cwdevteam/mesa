5ed8bc947bf08e73921a0e22d1a64f26320dad34
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-27 14:32:54 -0400

    I fix build errors.

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,12 +1,11 @@
 import LocaleProvider from "@/context/LocaleContext";
 import DictionaryProvider from "@/context/DictionaryContext";
 import ThemeProvider from "@/context/ThemeProvider";
-
 import { getDictionary } from "@/lib/dictionary";
 import { Locale } from "@/../i18n.config";
 import EASClientProvider from "./EASClientProvider";
 import WagmiProvider from "./WagmiProvider";
-import { UserProvider } from "./UserProvider";
+import UserProvider from "./UserProvider";
 
 export default async function Providers({
   children,
