56b476ba5f92404f0756f6f1381a630b227e7a90
HakobYeroyanEst <hakobyeroyan.dev@gmail.com>
2024-06-27 14:29:00 +0400

    fix: revert changes for dashboard and profile

--- src/app/[lang]/(protected)/dashboard/page.tsx
+++ src/app/[lang]/(protected)/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import DashboardPage from "@/components/DashboardPage";
+import { getUser, useSupabase } from "@/lib/supabase/server";
 
-const Dashboard = () => {
-  return <DashboardPage />;
+const Dashboard = async () => {
+  const supabase = useSupabase();
+  const user = await getUser(supabase);
+
+  return <DashboardPage email={user?.email} />;
 };
 
 export default Dashboard;
--- src/app/[lang]/(protected)/profile/page.tsx
+++ src/app/[lang]/(protected)/profile/page.tsx
@@ -1,5 +1,11 @@
 import ProfilePage from "@/components/ProfilePage";
+import { useSupabase } from "@/lib/supabase/server";
 
-export default function Profile() {
-  return <ProfilePage />;
+export default async function Profile() {
+  const supabase = useSupabase();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return <ProfilePage user={user} />;
 }
