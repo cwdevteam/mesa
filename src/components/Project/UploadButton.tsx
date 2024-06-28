b76ee41929ade99bc444868dfea79ff3014f75db
HakobYeroyanEst <hakobyeroyan.dev@gmail.com>
2024-06-28 17:37:04 +0400

    fix: resolve conversations

--- src/components/Project/UploadButton.tsx
+++ src/components/Project/UploadButton.tsx
@@ -1,26 +1,17 @@
 "use client";
-import { useState } from "react";
 import { Button } from "../ui/button";
-import { FilePlusIcon, ReloadIcon } from "@radix-ui/react-icons";
+import { FilePlusIcon } from "@radix-ui/react-icons";
 
 const UploadButton = ({ projectId }: { projectId: string }) => {
-  const [loading, setLoading] = useState<boolean>(false);
-
   return (
     <div>
-      {loading ? (
-        <Button size="icon" className="rounded-full">
-          <ReloadIcon color="currentColor" className="h-4 w-4 animate-spin" />
-        </Button>
-      ) : (
-        <Button
-          size="icon"
-          className="rounded-full"
-          onClick={() => document.getElementById("fileUpload")?.click()}
-        >
-          <FilePlusIcon className="h-4 w-4" />
-        </Button>
-      )}
+      <Button
+        size="icon"
+        className="rounded-full"
+        onClick={() => document.getElementById("fileUpload")?.click()}
+      >
+        <FilePlusIcon className="h-4 w-4" />
+      </Button>
       <input
         type="file"
         id="fileUpload"
