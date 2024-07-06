b5d6b399c1b8ae5fb834107241894e4fc05fef83
Tayyab Shahid <34285753+tayyabshahid7@users.noreply.github.com>
2024-07-06 11:17:02 +0500

    add the reference attestation to the existing attest call done

--- src/hooks/usePaymasterAttest.tsx
+++ src/hooks/usePaymasterAttest.tsx
@@ -5,11 +5,13 @@ import getAttestArgs from "@/lib/eas/getAttestArgs";
 import getEncodedAttestationData from "@/lib/eas/getEncodedAttestationData";
 import { Address } from "viem";
 import { useAccount } from "wagmi";
+import { useParams } from 'next/navigation'
 
 const usePaymasterAttest = () => {
   const { name, description } = useProjectProvider();
   const { writeContracts, capabilities } = usePaymasterProvider();
   const { address } = useAccount();
+  const {id} = useParams()
 
   const attest = async () => {
     const encodedAttestation = getEncodedAttestationData(
@@ -19,7 +21,7 @@ const usePaymasterAttest = () => {
       [address as Address],
       []
     );
-    const args = getAttestArgs(encodedAttestation);
+    const args = getAttestArgs(encodedAttestation, id);
     easAttest(writeContracts, capabilities, args);
   };
 
