c1899a629c7d64bbb9868b8688308320da9401dc
Tayyab Shahid <34285753+tayyabshahid7@users.noreply.github.com>
2024-07-18 13:48:02 +0500

    pr update fix

--- src/app/api/collaborators/addCollaborator.tsx
+++ src/app/api/collaborators/addCollaborator.tsx
@@ -1,21 +1,11 @@
 import handleError from "./handleError";
 import { NextRequest, NextResponse } from "next/server";
-import { getNodeMailerClient } from "@/lib/nodemailer/nodemailer";
-import { getEmailHtmlText } from "@/lib/nodemailer/getBodyText";
-import { getEmailOptions } from "@/lib/nodemailer/getEmailOptions";
 import { addNewCollaborator } from "@/lib/supabase/collaborators/addCollaborator";
 
 const addCollaboratorApi = async (req: NextRequest) => {
   try {
     const data = await req.json();
     const response: any = await addNewCollaborator(data);
-    const transporter = getNodeMailerClient();
-    const emailHtml = getEmailHtmlText(data.username, data.description, "");
-
-    try {
-      await transporter.sendMail(getEmailOptions(data.email, emailHtml));
-      response["to"] = data.email;
-    } catch (error) {}
 
     return NextResponse.json(response, { status: 200 });
   } catch (error: any) {
