87406b2cce48b862186bc4011aaa4c0bf48568a5
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-18 22:02:18 -0400

    I init shared ProjectMetadataSelect component.

--- src/components/Forms/ProjectMetadataForm/ContractTypeSelect.tsx
+++ src/components/Forms/ProjectMetadataForm/ContractTypeSelect.tsx
@@ -1,42 +1,21 @@
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { ContractType, contractTypeOptions } from "@/types/projectMetadataForm";
+import ProjectmetadataSelect from "./ProjectMetadataSelect";
 
 const ContractTypeSelect = () => {
   const contractType = ContractType.Songwriting;
+  const label = contractTypeOptions.find(
+    (v) => v.value === contractType
+  )?.label;
 
   return (
     <div className="grid gap-3">
       <Label htmlFor="title">Contract Type</Label>
-      <Select value={contractType}>
-        <SelectTrigger className="bg-card border-none">
-          <div className="flex items-center gap-2">
-            <SelectValue className="text-sm font-semibold">
-              {contractTypeOptions.find((v) => v.value === contractType)?.label}
-            </SelectValue>
-          </div>
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {contractTypeOptions.map(({ value, label }) => (
-              <SelectItem key={value} value={value}>
-                <div className="flex items-center gap-2">
-                  <span className="text-sm font-semibold">
-                    {label ?? value}
-                  </span>
-                </div>
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+      <ProjectmetadataSelect
+        value={contractType}
+        label={label}
+        options={contractTypeOptions}
+      />
     </div>
   );
 };
--- src/components/Forms/ProjectMetadataForm/UserRoleSelect.tsx
+++ src/components/Forms/ProjectMetadataForm/UserRoleSelect.tsx
@@ -1,42 +1,19 @@
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { UserRole, userRoleOptions } from "@/types/projectMetadataForm";
+import ProjectmetadataSelect from "./ProjectMetadataSelect";
 
 const UserRoleSelect = () => {
   const userRole = UserRole.Artist;
+  const label = userRoleOptions.find((v) => v.value === userRole)?.label;
 
   return (
     <div className="grid gap-3">
       <Label htmlFor="title">User Role</Label>
-      <Select value={userRole}>
-        <SelectTrigger className="bg-card border-none">
-          <div className="flex items-center gap-2">
-            <SelectValue className="text-sm font-semibold">
-              {userRoleOptions.find((v) => v.value === userRole)?.label}
-            </SelectValue>
-          </div>
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {userRoleOptions.map(({ value, label }) => (
-              <SelectItem key={value} value={value}>
-                <div className="flex items-center gap-2">
-                  <span className="text-sm font-semibold">
-                    {label ?? value}
-                  </span>
-                </div>
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+      <ProjectmetadataSelect
+        value={userRole}
+        label={label}
+        options={userRoleOptions}
+      />
     </div>
   );
 };
