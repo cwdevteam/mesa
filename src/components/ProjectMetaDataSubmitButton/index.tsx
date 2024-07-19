e8d1c09468e181b511bc42902c4a987575d57ac7
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-18 22:08:27 -0400

    I simplify ProjectMetaDataSubmitButton.

--- src/components/ProjectMetaDataSubmitButton/index.tsx
+++ src/components/ProjectMetaDataSubmitButton/index.tsx
@@ -1,28 +1,23 @@
-import { Pencil1Icon, ReloadIcon } from '@radix-ui/react-icons'
-import { Button } from '@/components/ui/button'
+import { Pencil1Icon, ReloadIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
 
-export default function ProjectMetaDataSubmitButton({
+const ProjectMetaDataSubmitButton = ({
   handleSubmit,
   loading,
-  request
+  request,
 }: {
-  handleSubmit: () => void
-  loading: boolean
-  request: string
-}) {
-  return (
-    <>
-      {loading ? (
-        <Button className="inline-flex gap-2" onClick={handleSubmit}>
-          <ReloadIcon color="currentColor" className="h-4 w-4 animate-spin" />
-          {request === "edit" ? "Updating..." : "Creating..."}
-        </Button>
-      ) : (
-        <Button className="inline-flex gap-2" onClick={handleSubmit}>
-          <Pencil1Icon color="currentColor" className="h-4 w-4" />
-          {request === "edit" ? "Update" : "Create"}
-        </Button>
-      )}
-    </>
-  )
-}
+  handleSubmit: () => void;
+  loading: boolean;
+  request: string;
+}) => (
+  <Button className="inline-flex gap-2" onClick={handleSubmit}>
+    {loading ? (
+      <Pencil1Icon color="currentColor" className="h-4 w-4" />
+    ) : (
+      <ReloadIcon color="currentColor" className="h-4 w-4 animate-spin" />
+    )}
+    {request === "edit" ? "Update" : "Create"}
+  </Button>
+);
+
+export default ProjectMetaDataSubmitButton;
