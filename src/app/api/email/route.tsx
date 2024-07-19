3b467ba533fa980ea60719f179bfd85683573ae5
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-18 20:34:48 -0400

    I remove email API.

--- src/app/api/email/route.tsx
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-import sendEmailApi from "@/app/api/email/sendEmail";
-
-export async function POST(req: NextRequest) {
-  return await sendEmailApi(req);
-}
--- src/app/api/invitations/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-import addInvitation from "@/app/api/invitations/addInvitation";
-
-export const runtime = "edge";
-
-export async function POST(req: NextRequest) {
-  return await addInvitation(req);
-}
