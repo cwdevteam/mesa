4d03440dbd0e3d2b2be388dbf1d4b73a157ceb57
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-19 09:39:34 -0400

    I refactor component definition to use const rather than function keyword.

--- src/components/ProjectInviteDialog/ProjectInviteDialog.tsx
+++ src/components/ProjectInviteDialog/ProjectInviteDialog.tsx
@@ -9,29 +9,25 @@ import {
 } from "@/components/ui/dialog";
 import ProjectInviteForm from "@/components/Forms/ProjectInviteForm";
 
-export default function ProjectInviteDialog({
-  children,
-  className,
-  ...props
-}: any) {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent
-        className={cn("grid gap-8 max-w-md px-8 py-16", className)}
-        {...props}
-      >
-        <DialogHeader>
-          <DialogTitle>Loop in the team</DialogTitle>
-          <DialogDescription>
-            Collaborators will be notified that you would like to be involved in
-            this work in progress.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="max-w-full">
-          <ProjectInviteForm />
-        </div>
-      </DialogContent>
-    </Dialog>
-  );
-}
+const ProjectInviteDialog = ({ children, className, ...props }: any) => (
+  <Dialog>
+    <DialogTrigger asChild>{children}</DialogTrigger>
+    <DialogContent
+      className={cn("grid gap-8 max-w-md px-8 py-16", className)}
+      {...props}
+    >
+      <DialogHeader>
+        <DialogTitle>Loop in the team</DialogTitle>
+        <DialogDescription>
+          Collaborators will be notified that you would like to be involved in
+          this work in progress.
+        </DialogDescription>
+      </DialogHeader>
+      <div className="max-w-full">
+        <ProjectInviteForm />
+      </div>
+    </DialogContent>
+  </Dialog>
+);
+
+export default ProjectInviteDialog;
