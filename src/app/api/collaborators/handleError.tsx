9556b2d6b9425452416c2e5c01d8ab29a6190592
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-18 20:30:08 -0400

    I remove collaborators API.

--- src/app/api/collaborators/handleError.tsx
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-const handleError = (message: string, status: number) =>
-  NextResponse.json({ error: message }, { status });
-
-export default handleError;
