08b888eca0f6fc9c2c03449cda36aafbeac5e653
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-19 10:36:15 -0400

    I remove dev logging.

--- src/app/api/email/route.tsx
+++ src/app/api/email/route.tsx
@@ -6,13 +6,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   try {
-    console.log("SENDING EMAIL");
-
     const body = await request.json();
-    console.log("Request Body:", body);
     const { email, name } = body;
-    console.log("Parsed Data:", { email, name });
-    console.log(email, name);
     if (!email || !name) {
       return new Response(JSON.stringify({ error: "Missing email or name" }), {
         status: 400,
