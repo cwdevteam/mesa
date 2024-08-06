493c2274474353212f50fe2e0d788e4bfd741f16
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-06 16:50:26 -0300

    I update distribution and project details pages so the player does not block buttons.

--- src/components/DistributionOnchain/ZoraTokenForm.tsx
+++ src/components/DistributionOnchain/ZoraTokenForm.tsx
@@ -2,48 +2,30 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 import type { Create1155TokenMutation } from "@/hooks/useZoraToken";
 import { useProjectProvider } from "@/context/ProjectProvider";
 import ZoraMediaFile from "./ZoraMediaFile";
 import CreateButton from "./CreateButton";
 import ZoraImageSelect from "./ZoraImageSelect";
 import { useOnchainDistributionProvider } from "@/context/OnchainDistributionProvider";
+import TitleAndDescription from "./TitleAndDescription";
 
 interface ZoraCardProps {
   create1155Token: Create1155TokenMutation;
 }
 
 export default function ZoraTokenForm({ create1155Token }: ZoraCardProps) {
-  const { name, description, animationUrl, setEthPrice } = useProjectProvider();
+  const { animationUrl, setEthPrice } = useProjectProvider();
   const { isZora } = useOnchainDistributionProvider();
 
   return (
     <div className="flex flex-col gap-8 max-w-md flex-1">
-      <div className="grid w-full items-center gap-2">
-        <Label htmlFor="name">Title:</Label>
-        <Input
-          type="text"
-          name="name"
-          id="name"
-          required
-          value={name}
-          disabled
-        />
-      </div>
-      <div className="grid w-full items-center gap-2">
-        <Label htmlFor="description">Description:</Label>
-        <Textarea
-          name="description"
-          id="description"
-          required
-          value={description}
-          disabled
-        />
-      </div>
+      <TitleAndDescription />
 
-      <ZoraImageSelect />
-      <ZoraMediaFile mediaFile={animationUrl} />
+      <div className="flex justify-between w-full">
+        <ZoraImageSelect />
+        <ZoraMediaFile mediaFile={animationUrl} />
+      </div>
       {isZora && (
         <div className="grid w-full items-center gap-2">
           <Label htmlFor="tokenPrice">Price per token (ETH):</Label>
