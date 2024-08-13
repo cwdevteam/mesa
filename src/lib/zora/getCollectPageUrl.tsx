3edfe38ed1d1f9d7520ee915701fc01a5cc8b455
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-13 11:46:47 -0300

    I fix zora collect links.

--- src/lib/zora/getCollectPageUrl.tsx
+++ src/lib/zora/getCollectPageUrl.tsx
@@ -2,8 +2,8 @@ import { Address } from "viem";
 import { IS_TESTNET } from "../consts";
 
 const getCollectPageUrl = (collectionAddress: Address) =>
-  `https://${
-    IS_TESTNET ? "testnet." : ""
-  }zora.co/collect/bsep:${collectionAddress}/1`;
+  `https://${IS_TESTNET ? "testnet." : ""}zora.co/collect/${
+    IS_TESTNET ? "bsep" : "base"
+  }:${collectionAddress}/1`;
 
 export default getCollectPageUrl;
