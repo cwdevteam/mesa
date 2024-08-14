9c753828e3f563f631aeaa18932e25ed69c5418d
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-14 18:14:00 -0300

    I update useZoreCreate hook to use new salesConfig dynamic selection.

--- src/hooks/useZoraSaleStrategy.tsx
+++ src/hooks/useZoraSaleStrategy.tsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useProjectProvider } from "@/context/ProjectProvider";
+import getSalesConfig from "@/lib/zora/getSalesConfig";
+import { useMemo, useState } from "react";
+import { parseEther } from "viem";
 
 const useZoraSaleStrategy = () => {
+  const { name, ethPrice } = useProjectProvider();
   const [zoraSaleStrategy, setZoraSaleStrategy] =
     useState<string>("fixedPrice");
   const isFixedPrice = zoraSaleStrategy === "fixedPrice";
   const isTimed = zoraSaleStrategy === "timed";
+  const salesConfig = useMemo(
+    () =>
+      getSalesConfig({
+        saleStrategy: zoraSaleStrategy,
+        erc20Name: name,
+        pricePerToken: parseEther(ethPrice.toString()),
+      }),
+    [ethPrice, name, zoraSaleStrategy]
+  );
 
-  return { isFixedPrice, isTimed, zoraSaleStrategy, setZoraSaleStrategy };
+  return {
+    isFixedPrice,
+    isTimed,
+    salesConfig,
+    zoraSaleStrategy,
+    setZoraSaleStrategy,
+  };
 };
 
 export default useZoraSaleStrategy;
