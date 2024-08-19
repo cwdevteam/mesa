a5a0aa673e9f96793313ab9db32294641e793cca
SeniorDev <tech.eng.me@gmail.com>
2024-08-19 13:44:12 -0400

    Update ProjectDetailsForm.tsx

--- src/components/NewProjectButton/ProjectDetailsForm.tsx
+++ src/components/NewProjectButton/ProjectDetailsForm.tsx
@@ -34,7 +34,6 @@ export default function ProjectDetailsForm() {
         description: "Failed to create project",
         variant: "default"
       })
-      return
     }
     setLoading(false)
   };
