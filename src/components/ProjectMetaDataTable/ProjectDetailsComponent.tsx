0a6a2eb69d02b728439eab903a2f403bc1b662ac
SeniorDev <tech.eng.me@gmail.com>
2024-08-18 23:43:00 -0400

    I remove chat

--- src/components/ProjectMetaDataTable/ProjectDetailsComponent.tsx
+++ src/components/ProjectMetaDataTable/ProjectDetailsComponent.tsx
@@ -1,4 +1,3 @@
-import Chat from '@/components/ProjectChatBox/Chat'
 import ProjectCollaborators from '../ProjectCollaborators'
 import ProjectDetailsCard from '../ProjectDetailsCard'
 import UploadButton from '../Project/UploadButton'
@@ -7,15 +6,10 @@ import ImageSelect from '../Project/ImageSelect'
 const ProjectDetailsComponent = () => (
   <div className="flex flex-col items-center lg:items-start gap-2 w-full">
     <ProjectDetailsCard />
-    <div className="flex flex-col lg:flex-row-reverse gap-8 w-full">
-      <div className="w-full">
-        <ProjectCollaborators />
-        <UploadButton />
-        <ImageSelect />
-      </div>
-      <div className="w-full lg:max-w-[400px]">
-        <Chat />
-      </div>
+    <div className="w-full">
+      <ProjectCollaborators />
+      <UploadButton />
+      <ImageSelect />
     </div>
   </div>
 )
