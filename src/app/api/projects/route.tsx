ee876bd277b02cb4c591d48c07e6022b7ae2ec4f
SeniorDev <tech.eng.me@gmail.com>
2024-08-20 15:45:22 -0400

    WIP

--- src/app/api/projects/route.tsx
+++ src/app/api/projects/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { findLatestProjectStates } from '@/lib/eas/findLatestProjectStates'
+import { IS_TESTNET } from '@/lib/consts'
 
 export const runtime = 'edge'
 
@@ -7,8 +8,11 @@ export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
     const address = searchParams.get('address')
-
+    const response = await fetch(
+      `https://base${IS_TESTNET ? '-sepolia' : ''}.easscan.org/address/${address}?_data=routes/__boundary/address/$address`
+    )
     const data = await response.json()
+    console.log("ZIAD", data.attestations)
     const attestations = findLatestProjectStates(data.attestations)
 
     return NextResponse.json({ data: attestations }, { status: 200 })
