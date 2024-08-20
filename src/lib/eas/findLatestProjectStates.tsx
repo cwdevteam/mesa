a9023a3ca181589e7dbf03ebbea7fbe863c26aee
SeniorDev <tech.eng.me@gmail.com>
2024-08-20 16:56:29 -0400

    I revert change

--- src/lib/eas/findLatestProjectStates.tsx
+++ src/lib/eas/findLatestProjectStates.tsx
@@ -1,42 +1,41 @@
 export const findLatestProjectStates = (attestations: any[]) => {
-  let uniqueProjectMap = new Map<string, any>();
-  let creationMap = new Map<string, any>();
+  let uniqueProjectMap = new Map<string, any>()
+  let creationMap = new Map<string, any>()
 
   for (let i = 0; i < attestations.length; i++) {
     const attestation = attestations[i];
     const refUID = attestation.refUID;
-
     const isProjectCreation =
       refUID ===
         '0x0000000000000000000000000000000000000000000000000000000000000000' ||
-      !refUID;
+      !refUID
+    const isProjectUpdate = !uniqueProjectMap.has(refUID)
 
     if (isProjectCreation) {
       const uid = attestation.id;
-      creationMap.set(uid, attestation);
+      creationMap.set(uid, attestation)
     } else {
-      if (!uniqueProjectMap.has(refUID)) {
-        uniqueProjectMap.set(refUID, attestation);
+      if (isProjectUpdate) {
+        uniqueProjectMap.set(refUID, attestation)
       } else {
-        const existingAttestation = uniqueProjectMap.get(refUID);
-        const isLatestUpdate =
-          attestation.timeCreated > existingAttestation.timeCreated;
+        const existingAttestation = uniqueProjectMap.get(refUID)
+        const isLatestUpdate = i < attestations.indexOf(existingAttestation)
         if (isLatestUpdate) {
-          uniqueProjectMap.set(refUID, attestation);
+          uniqueProjectMap.set(refUID, attestation)
         }
       }
     }
   }
 
-  let finalResults: any[] = [];
+  let finalResults: any[] = []
 
   creationMap.forEach((creationAttestation, uid) => {
     if (uniqueProjectMap.has(uid)) {
-      finalResults.push(uniqueProjectMap.get(uid));
+      finalResults.push(uniqueProjectMap.get(uid))
     } else {
-      finalResults.push(creationAttestation);
+      finalResults.push(creationAttestation)
     }
-  });
+  })
 
-  return finalResults;
-};
+  return finalResults
+}
