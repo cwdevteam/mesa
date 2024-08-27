81e2bbea5a2426c53bd17cff5048e1390c644dad
SeniorDev <tech.eng.me@gmail.com>
2024-08-26 23:40:11 -0400

    I fix bug

--- src/lib/eas/findLatestReferences.tsx
+++ src/lib/eas/findLatestReferences.tsx
@@ -2,6 +2,11 @@ import { IS_TESTNET } from '../consts'
 
 const findLatestReferences = async (attestations: any) => {
   const latestPromise = attestations.map(async (attestation: any) => {
+    if (
+      attestation.refUID ===
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+    )
+      return attestation
     const response = await fetch(
       `https://base${IS_TESTNET ? '-sepolia' : ''}.easscan.org/attestations/referencing/${attestation.refUID}?_data=routes/__boundary/attestations/referencing/$attestationUID`
     )
